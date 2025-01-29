e0899965f60dc494628db51c2665b747caf3fe42
MiswToLinux1234 <tsapralis.th@gmail.com>
2025-01-29 14:41:32 +0200

    Update Synarthseis.h

--- Synarthseis.h
+++ Synarthseis.h
@@ -2,3 +2,14 @@
 
 //arxikopoiihsh katalogou
 arxikopoihsh_catalog()
+
+//gia thn diaxeirhsh paraggelias
+diax_paraggelias()
+
+// gia thn diaxeirhsh tou pelath
+*diax_pelath()
+
+//diergasia tou pelath
+  *client_thread()
+
+  
